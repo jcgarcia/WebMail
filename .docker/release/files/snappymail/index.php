045fad1c1298321ae79efd8781f7fa0ed49a84b6
jcgarcia <jcgarcia@ingasti.com>
2025-12-14 21:11:22 +0000

    Update: 2025-12-14 21:11

--- .docker/release/files/snappymail/index.php
+++ .docker/release/files/snappymail/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// SnappyMail Bootstrap
-// This file initializes the application and sets up the include path
+// SnappyMail Bootstrap for flattened directory structure
+// After flattening from snappymail/v/VERSION/ to root, this bootstraps the app
 
 if (!defined('APP_VERSION'))
 {
@@ -9,14 +9,14 @@ if (!defined('APP_VERSION'))
 	define('APP_INDEX_ROOT_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 }
 
-// Include the application bootstrap
-if (file_exists(APP_INDEX_ROOT_PATH.'app/index.php'))
+// After flattening, include.php is now at root level
+if (file_exists(APP_INDEX_ROOT_PATH.'include.php'))
 {
-	require APP_INDEX_ROOT_PATH.'app/index.php';
+	include APP_INDEX_ROOT_PATH.'include.php';
 }
 else
 {
-	echo '[105] Missing '.APP_INDEX_ROOT_PATH.'app/index.php';
+	echo '[105] Missing '.APP_INDEX_ROOT_PATH.'include.php';
 	is_callable('opcache_invalidate') && opcache_invalidate(__FILE__, true);
 	exit(105);
 }
